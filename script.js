0096b8309d753e550af8ab223717867c4439e970
Marouane-bichara <bicharamarouane0@gmail.com>
2024-10-19 02:26:02 +0200

    header

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let menu = document.getElementById('menu-icon');
 let navlist = document.querySelector('.alllist');
 
 
-navlist.addEventListener('click',()=>
+menu.addEventListener('click',()=>
 {
     menu.classList.toggle("bx-x");
     navlist.classList.toggle("open");
