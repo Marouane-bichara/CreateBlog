b4963973e171e03d783b4b21dc09b18afb68f23c
Marouane-bichara <bicharamarouane0@gmail.com>
2024-10-19 02:21:49 +0200

    Header

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu-icon');
+let navlist = document.querySelector('.alllist');
+
+
+navlist,addEventListener('click',()=>
+{
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+);
